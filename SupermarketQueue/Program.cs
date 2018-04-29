3fb519dce54cd9d1279b9a1f2905b159a388423d
AleksPeneva <alexemilpeneva@abv.bg>
2018-04-29 23:16:15 +0300

    Set up for working with linked list

--- SupermarketQueue/Program.cs
+++ SupermarketQueue/Program.cs
@@ -1,15 +1,84 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SupermarketQueue
 {
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
         }
+
+        internal void Insert(int position, string name, SingleLinkedList queue)
+        {
+            if (position == 0)
+            {
+                InsertHead(position, name, queue);
+            }
+            else
+            {
+
+            }
+        }
+
+        internal void InsertHead(int position, string name, SingleLinkedList queue)
+        {
+            Node newNode = new Node(name)
+            {
+                Next = queue.head
+            };
+            queue.head = newNode;
+            Console.WriteLine("OK");
+        }
+
+        internal void InsertTail(SingleLinkedList queue, string name)
+        {
+            Node newNode = new Node(name);
+            if (queue.head == null)
+            {
+                queue.head = newNode;
+                return;
+            }
+            Node tail = GetTail(queue);
+            tail.Next = newNode;
+        }
+
+        internal void InsertAt(int position, string name, SingleLinkedList list)
+        {
+            Node newNode = new Node(name)
+            {
+                Next = list.head
+            };
+            list.head = newNode;
+            Console.WriteLine("OK");
+        }
+
+        internal Node GetTail(SingleLinkedList queue)
+        {
+            Node temp = queue.head;
+            while (temp.Next != null)
+            {
+                temp = temp.Next;
+            }
+            return temp;
+        }
     }
+
+    internal class Node
+    {
+        private string name;
+        private Node next;
+        public Node(string n)
+        {
+            this.name = n;
+            this.next = null;
+        }
+
+        public Node Next { get; set; }
+    }
+
+    internal class SingleLinkedList
+    {
+        internal Node head;
+    }
+
 }
