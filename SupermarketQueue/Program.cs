bc651ec351673b58501d4628590dec7699accdf9
AleksPeneva <alexemilpeneva@abv.bg>
2018-04-30 01:23:58 +0300

    New concept for solution - use BigList

--- SupermarketQueue/Program.cs
+++ SupermarketQueue/Program.cs
@@ -2,15 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Wintellect.PowerCollections;
 
 namespace SupermarketQueue
 {
     public class Program
     {
+        private static StringBuilder output = new StringBuilder();
+        private static Bag<string> customers = new Bag<string>();
+        private static BigList<string> queue = new BigList<string>();
+        
         static void Main(string[] args)
         {
-            StringBuilder output = new StringBuilder();
-            SingleLinkedList queue = new SingleLinkedList();
             string input = Console.ReadLine();
             while (input != "End")
             {
@@ -18,18 +21,16 @@ namespace SupermarketQueue
                 switch (command[0])
                 {
                     case "Append":
-                        InsertTail(queue, command[1], output);
+                        
                         break;
                     case "Insert":
-                        int position = int.Parse(command[1]);
-                        Insert(position, command[2], queue, output);
+
                         break;
                     case "Find":
-                        Find(command[1], queue, output);
+                        
                         break;
                     case "Serve":
-                        int count = int.Parse(command[1]);
-                        Serve(count, queue, output);
+
                         break;
                     case "End":
                         break;
@@ -43,86 +44,12 @@ namespace SupermarketQueue
             Console.WriteLine(output);
         }
 
-        internal static void Serve(int count, SingleLinkedList queue, StringBuilder output)
+        private static void Append(string name)
         {
-
+            queue.Add(name);
+            customers.Add(name);
+            output.AppendLine("OK");
         }
 
-        internal static void Find(string name, SingleLinkedList queue, StringBuilder output)
-        {
-
-        }
-
-        internal static void Insert(int position, string name, SingleLinkedList queue, StringBuilder output)
-        {
-            if (position == 0)
-            {
-                InsertHead(position, name, queue, output);
-            }
-            else
-            {
-
-            }
-        }
-
-        internal static void InsertHead(int position, string name, SingleLinkedList queue, StringBuilder output)
-        {
-            Node newNode = new Node(name)
-            {
-                Next = queue.head
-            };
-            queue.head = newNode;
-            Console.WriteLine("OK");
-        }
-
-        internal static void InsertTail(SingleLinkedList queue, string name, StringBuilder output)
-        {
-            Node newNode = new Node(name);
-            if (queue.head == null)
-            {
-                queue.head = newNode;
-                return;
-            }
-            Node tail = GetTail(queue);
-            tail.Next = newNode;
-        }
-
-        internal static void InsertAt(int position, string name, SingleLinkedList list, StringBuilder output)
-        {
-            Node newNode = new Node(name)
-            {
-                Next = list.head
-            };
-            list.head = newNode;
-            Console.WriteLine("OK");
-        }
-
-        internal static Node GetTail(SingleLinkedList queue)
-        {
-            Node temp = queue.head;
-            while (temp.Next != null)
-            {
-                temp = temp.Next;
-            }
-            return temp;
-        }
     }
-
-    internal class Node
-    {
-        private string name;
-        public Node(string n)
-        {
-            this.name = n;
-            this.Next = null;
-        }
-
-        public Node Next { get; set; }
-    }
-
-    internal class SingleLinkedList
-    {
-        internal Node head;
-    }
-
 }
