0f8707ec48981ffefb5ec2dc8ea16c8ec869fa08
AleksPeneva <alexemilpeneva@abv.bg>
2018-04-30 23:12:10 +0300

    Methods to add combinations

--- GirlsGoneWild/Program.cs
+++ GirlsGoneWild/Program.cs
@@ -7,22 +7,45 @@ namespace GirlsGoneWild
     class Program
     {
         private static int numberOfGirls;
-        private List<List<int>> combOfNumbers = new List<List<int>>();  // lists of combinations
+        private static List<List<int>> combOfNumbers = new List<List<int>>();  // lists of number combinations
+        private static List<List<char>> combOfLetters = new List<List<char>>();  // lists of letter combinations
+        private static char[] letters;
+
         internal static void Main(string[] args)
         {
             int n = int.Parse(Console.ReadLine());
             int[] numbers =new int[n];
-            var letters = Console.ReadLine().ToCharArray().OrderBy(l => l);
+            letters = Console.ReadLine().ToCharArray().OrderBy(l => l).ToArray();
             numberOfGirls = int.Parse(Console.ReadLine());
 
             Combinations(numbers, 0, 0);
+            numbers = new int[letters.Length];
+            Combinations(numbers, 0, 0);
+        }
+
+        private static void AddNumberCombinations(int[] arr)
+        {
+            foreach (var item in arr)
+            {
+                combOfNumbers.Add(new List<int>(item));
+            }
+        }
+
+        private static void AddLetterCombinations(int[] arr)
+        {
+            List<char> curCombination = new List<char>();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                curCombination.Add(letters[arr[i]]);
+            }
+            combOfLetters.Add(curCombination);
         }
 
         internal static void Combinations(int[] arr, int start, int index)
         {
             if (index > numberOfGirls)
             {
-
+                AddNumberCombinations(arr);
             }
             else
             {
