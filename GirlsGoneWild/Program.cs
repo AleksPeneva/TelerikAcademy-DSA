2bdf9cdbadd0ac5116f3d4988eb893ecf03da33a
AleksPeneva <alexemilpeneva@abv.bg>
2018-04-30 21:29:27 +0300

    Putting in use Combination func

--- GirlsGoneWild/Program.cs
+++ GirlsGoneWild/Program.cs
@@ -1,15 +1,38 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace GirlsGoneWild
 {
     class Program
     {
+        private static int numberOfGirls;
+        private List<List<int>> combOfNumbers = new List<List<int>>();  // lists of combinations
         internal static void Main(string[] args)
         {
-            int numbers = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+            int[] numbers =new int[n];
             var letters = Console.ReadLine().ToCharArray().OrderBy(l => l);
-            int girls = int.Parse(Console.ReadLine());
+            numberOfGirls = int.Parse(Console.ReadLine());
+
+            Combinations(numbers, 0, 0);
+        }
+
+        internal static void Combinations(int[] arr, int start, int index)
+        {
+            if (index > numberOfGirls)
+            {
+
+            }
+            else
+            {
+                for (int i = start; i < arr.Length; i++)
+                {
+                    arr[index] = i;
+                    Combinations(arr, index+1, i+1);
+                }
+            }
+
         }
     }
 }
