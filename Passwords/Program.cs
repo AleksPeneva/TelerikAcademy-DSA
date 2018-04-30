2217142d0f7562fd54aeccb718d604df7ddc61e6
AleksPeneva <alexemilpeneva@abv.bg>
2018-04-30 13:18:31 +0300

    < case covered

--- Passwords/Program.cs
+++ Passwords/Program.cs
@@ -10,17 +10,24 @@ namespace Passwords
             string relations = Console.ReadLine();
             int k = int.Parse(Console.ReadLine());
             var output = new char[length];
-            string keyboard = "1234567890";
+            int[] keyboard = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
             Hack(relations, length, k, 0, keyboard);
 
             Console.WriteLine(output);
         }
 
-        public static int Hack(string relations, int length, int k, int index, string keyboard)
+        public static int Hack(string relations, int length, int k, int index, int[] keyboard)
         {
             if (relations[index] == '<')
             {
+                index = keyboard[index] == 0 ? 10 : keyboard[index];
+                for (int i = 0; i < length; i++)
+                {
+                    keyboard[index] = i;
+                    length = Hack(relations, length, k, index, keyboard);
+                }
 
+                return length;
             }
 
             if (relations[index] == '>')
