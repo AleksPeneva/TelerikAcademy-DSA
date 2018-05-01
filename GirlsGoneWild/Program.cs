8d598144be0e8f31e4b25edabee85c19f0bf6f8f
AleksPeneva <alexemilpeneva@abv.bg>
2018-05-02 00:21:21 +0300

    Set up input for GirlsGoneWild

--- GirlsGoneWild/Program.cs
+++ GirlsGoneWild/Program.cs
@@ -1,7 +1,20 @@
-﻿namespace GirlsGoneWild
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GirlsGoneWild
 {
 	class Program
 	{
+		private static int numbers;
+		private static char[] letters;
+		private static int girlsCount;
 
+		static void Main(string[] args)
+		{
+			numbers = int.Parse(Console.ReadLine());
+			letters = Console.ReadLine().ToCharArray().OrderBy(l => l).ToArray();
+			girlsCount = int.Parse(Console.ReadLine());
+		}
 	}
 }
