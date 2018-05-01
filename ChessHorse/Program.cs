c5a5de826681f2de4f848b4d5ff3cf0ad553b357
AleksPeneva <alexemilpeneva@abv.bg>
2018-05-01 19:55:46 +0300

    ChessHorse: set up input and printing output

--- ChessHorse/Program.cs
+++ ChessHorse/Program.cs
@@ -1,15 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ChessHorse
 {
-    class Program
+	class Program
     {
+		private static int[,] matrix;
         static void Main(string[] args)
         {
-        }
-    }
+			int rows = int.Parse(Console.ReadLine());
+			int cols = int.Parse(Console.ReadLine());
+			int startRow = int.Parse(Console.ReadLine());
+			int startCol = int.Parse(Console.ReadLine());
+
+			int[] outputCol = new int[rows];
+			matrix = new int[rows, cols];
+
+			BFS();
+
+			PrintOutput(outputCol);
+		}
+
+		private static void PrintOutput(int[] outputCol)
+		{
+			for (int i = 0; i < outputCol.Length; i++)
+			{
+				Console.WriteLine(outputCol[i]);
+			}
+		}
+
+		private static void BFS()
+		{
+			throw new NotImplementedException();
+		}
+	}
 }
