ed046c96f8b6236d077277105a69c54c7fc29146
AleksPeneva <alexemilpeneva@abv.bg>
2018-05-02 00:38:56 +0300

    Organizing input data

--- GirlsGoneWild/Program.cs
+++ GirlsGoneWild/Program.cs
@@ -9,12 +9,28 @@ namespace GirlsGoneWild
 		private static int numbers;
 		private static char[] letters;
 		private static int girlsCount;
+		private static string[] combo;
+		private static Dictionary<char, int> letterRepetitions = new Dictionary<char, int>();
 
 		static void Main(string[] args)
+		{
+			OrganizeData();
+		}
+
+		private static void OrganizeData()
 		{
 			numbers = int.Parse(Console.ReadLine());
 			letters = Console.ReadLine().ToCharArray().OrderBy(l => l).ToArray();
 			girlsCount = int.Parse(Console.ReadLine());
+			combo = new string[girlsCount];
+			foreach (var letter in letters)
+			{
+				if (!letterRepetitions.ContainsKey(letter))
+				{
+					letterRepetitions.Add(letter, 0);
+				}
+				letterRepetitions[letter]++;
+			}
 		}
 	}
 }
