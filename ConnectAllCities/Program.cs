dcf349cb0e721552bd57c9f90fdeb78db2f4fb1d
AleksPeneva <alexemilpeneva@abv.bg>
2018-05-02 20:07:37 +0300

    Setting up input for ConnectAllCities

--- ConnectAllCities/Program.cs
+++ ConnectAllCities/Program.cs
@@ -1,15 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConnectAllCities
 {
-    class Program
+	class Program
     {
+		private static int[,] initialCityConnections;
+
         static void Main(string[] args)
         {
+			int numberOfTests = int.Parse(Console.ReadLine());
+			for (int i = 0; i < numberOfTests; i++)
+			{
+				Test();
+			}
         }
+
+		private static void Test()
+		{
+			int minTransformations;
+			int numberOfCities = int.Parse(Console.ReadLine());
+			FillMatrix(numberOfCities);
+
+
+			Console.WriteLine(minTransformations);
+		}
+
+		private static void FillMatrix(int numberOfCities)
+		{
+			for (int i = 0; i < numberOfCities; i++)
+			{
+				int[] line = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+				for (int j = 0; j < numberOfCities; j++)
+				{
+					initialCityConnections[i, j] = line[j];
+				}
+			}
+		}
     }
 }
